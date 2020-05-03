3b676fc5a106544d77678806ed48e996ad2ae890
tinfoil-knight <kunal99kundu@gmail.com>
2020-05-03 19:18:16 +0530

    GoWiki: Using httptest

--- gowiki/wiki_test.go
+++ gowiki/wiki_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -11,7 +13,7 @@ import (
 func createPage(title string, body []byte) {
 	p1 := &Page{Title: title, Body: body}
 	p1.save()
-	fmt.Printf("HELPER: Created %s.txt\n", title)
+	fmt.Printf("+++ HELPER: created %s.txt\n", title)
 }
 
 func deleteFile(filename string) {
@@ -26,17 +28,20 @@ func deleteFile(filename string) {
 	if removeErr != nil {
 		fmt.Println(removeErr)
 	}
-	fmt.Printf("HELPER: %s.txt deleted\n", filename)
+	fmt.Printf("+++ HELPER: %s.txt deleted\n", filename)
+}
+
+func runServer(fn func(http.ResponseWriter, *http.Request, string)) *httptest.Server {
+	return httptest.NewServer(makeHandler(fn))
 }
 
 // Tests
 
-func Test_loadPage(t *testing.T) {
+func Test__PageLoad(t *testing.T) {
 	// Test Config
 	title := "test"
 	body := []byte("This is a sample page.")
 	// Test Setup
-	deleteFile(title)
 	createPage(title, body)
 	// Test Run
 	page, err := loadPage(title)
@@ -50,4 +55,24 @@ func Test_loadPage(t *testing.T) {
 	if string(page.Body) != "This is a sample page." {
 		t.Errorf("Expected page.Body to be: \"%s\" but got: \"%s\"", body, page.Body)
 	}
+}
+
+func Test__ViewHandler(t *testing.T) {
+	// Test Config
+	title := "test"
+	body := []byte("This is a sample page.")
+	// Test Setup
+	createPage(title, body)
+	ts := runServer(viewHandler)
+	// Test Run
+	url := fmt.Sprintf("%s/view/%s", ts.URL, title)
+	res, getErr := http.Get(url)
+	if getErr != nil {
+		fmt.Println(getErr)
+	}
+	if res.StatusCode != 200 {
+		t.Errorf("Expected StatusCode : 200 , Received StatusCode: %v", res.StatusCode)
+	}
+
+	ts.Close()
 }
